15f6d1d7ec75969ff2ecf04a336f85cc6256f27c
Kingsley Ekoh-Ordan <keordan@sbsc.com>
2021-10-31 19:17:48 +0100

    feat: implement text file reader

---
+++ EmailAddressExtractorSln/DocumentReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailAddressExtractorSln
+{
+    public static class DocumentReader
+    {
+        public static string ReadTextFile(string filePath)
+        {
+            try
+            {
+                return File.ReadAllText(filePath);
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static string ReadWordFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
