c6d1fc2a47c7d32fbbfa4e7a495ba500edc44b3c
Kingsley Ekoh-Ordan <keordan@sbsc.com>
2021-10-31 17:22:28 +0100

    init

---
+++ EmailAddressExtractorSln/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailAddressExtractorSln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
